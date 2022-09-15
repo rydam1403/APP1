af4008c5da757e1c5fbc1fc76a30f1e9a7674261
rydam1403 <rydam1911007@akgec.ac.in>
2022-09-16 00:07:07 +0530

    Initial commit

---
+++ lib/homepage.dart
@@ -0,0 +1,318 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:smooth_star_rating/smooth_star_rating.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: ListView(
+        children: <Widget>[
+          Container(
+            height: MediaQuery.of(context).size.height / 3.1,
+            decoration: BoxDecoration(
+                color: Colors.blueAccent,
+                borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(20.0),
+                    bottomRight: Radius.circular(20.0))),
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(top: 5.0, right: 10.0),
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: Image(
+                      image: AssetImage('images/foodlogo.png'),
+                      width: 100.0,
+                      height: 50.0,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                Container(
+                  height: 120.0,
+                  child: ListView(
+                    shrinkWrap: true,
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      Expanded(
+                        child: upperMenu(ititle: 'drink2', iname: 'DRINK'),
+                      ),
+                      SizedBox(
+                        width: 18.0,
+                      ),
+                      Expanded(
+                        child: upperMenu(ititle: 'pizza', iname: 'PIZZA'),
+                      ),
+                      SizedBox(
+                        width: 18.0,
+                      ),
+                      Expanded(
+                        child: upperMenu(ititle: 'burger2', iname: 'BURGER'),
+                      ),
+                      SizedBox(
+                        width: 18.0,
+                      ),
+                      Expanded(
+                        child:
+                            upperMenu(ititle: 'sandwitch2', iname: 'SANDWICH'),
+                      ),
+                      SizedBox(
+                        width: 18.0,
+                      ),
+                      Expanded(
+                        child:
+                            upperMenu(ititle: 'icecream', iname: 'ICE-CREAM'),
+                      ),
+                      SizedBox(
+                        width: 18.0,
+                      ),
+                      Expanded(
+                        child: upperMenu(ititle: 'noodles', iname: 'NOODLES'),
+                      ),
+                      SizedBox(
+                        width: 3.0,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 30.0, left: 16.0),
+            height: 40.0,
+            child: ListView(
+              shrinkWrap: true,
+              scrollDirection: Axis.horizontal,
+              children: <Widget>[
+                bottomMenu(menu: 'CAKE', c: 0xFFF484B1),
+                SizedBox(
+                  width: 30.0,
+                ),
+                bottomMenu(menu: 'PIE', c: 0xFFAB47BC),
+                SizedBox(
+                  width: 30.0,
+                ),
+                bottomMenu(menu: 'ICE-CREAM', c: 0xFF7986CB),
+                SizedBox(
+                  width: 30.0,
+                ),
+                bottomMenu(menu: 'CANNOLI', c: 0xFF40C4FF),
+                SizedBox(
+                  width: 10.0,
+                ),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 34.0,
+          ),
+          Container(
+            alignment: Alignment.centerLeft,
+            padding: EdgeInsets.only(left: 30.0),
+            child: Text(
+              'Most Popular',
+              style: TextStyle(
+                color: Colors.black,
+                letterSpacing: 2.2,
+                fontWeight: FontWeight.bold,
+                fontSize: 30.0,
+              ),
+            ),
+          ),
+          dishCard(context: context),
+          SizedBox(
+            height: 30.0,
+          ),
+          dishCard(context: context),
+          SizedBox(
+            height: 30.0,
+          ),
+          dishCard(context: context),
+          SizedBox(
+            height: 30.0,
+          ),
+          dishCard(context: context),
+        ],
+      ),
+    ));
+  }
+
+  Widget upperMenu({String ititle, String iname}) {
+    return Container(
+      margin: EdgeInsets.only(left: 10.0, bottom: 10.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 40.0,
+            child: Image(
+              image: AssetImage('images/$ititle.png'),
+              height: 43.0,
+            ),
+          ),
+          SizedBox(
+            height: 4.0,
+          ),
+          Text(
+            '$iname',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 15.0,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget bottomMenu({String menu, int c}) {
+    return Container(
+      height: 20.0,
+      child: RaisedButton(
+        color: Color(c),
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(22.6)),
+        child: Text(
+          '$menu',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18.0,
+          ),
+        ),
+        onPressed: () {},
+      ),
+    );
+  }
+
+  rate({double s}) {
+    return SmoothStarRating(
+        allowHalfRating: true,
+        starCount: 5,
+        rating: s,
+        size: 20.0,
+        isReadOnly: false,
+        color: Colors.yellow,
+        borderColor: Colors.grey,
+        spacing: 0.0);
+  }
+
+  dishCard({BuildContext context}){
+    return Container(
+      width: MediaQuery.of(context).size.width / 1.1,
+      margin: EdgeInsets.only(left: 30.0, right: 30.0),
+      child: Card(
+        elevation: 2.0,
+        child: Container(
+          decoration: BoxDecoration(
+              border: Border.all(
+                color: Colors.grey,
+              )),
+          child: Column(
+            children: <Widget>[
+              Container(
+                decoration: BoxDecoration(
+                    border:
+                    Border.all(color: Colors.grey[400], width: 1.5)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(
+                              top: 5.0, left: 12.0, bottom: 5.0),
+                          child: CircleAvatar(
+                            radius: 20.0,
+                            backgroundImage:
+                            AssetImage('images/vchef.jfif'),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text(
+                          'Olea Powers',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16.0,
+                          ),
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          margin: EdgeInsets.only(top: 3.0),
+                          child: Icon(
+                            Icons.favorite,
+                            color: Colors.red,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Container(
+                          margin: EdgeInsets.only(top: 3.0, right: 15.0),
+                          child: Text(
+                            '160',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 3.0,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    border:
+                    Border.all(color: Colors.grey[400], width: 1.5)),
+                width: MediaQuery.of(context).size.width,
+                child: Image(image: AssetImage('images/plook.png')),
+              ),
+              SizedBox(
+                height: 3.0,
+              ),
+              Container(
+                decoration: BoxDecoration(
+                    border:
+                    Border.all(color: Colors.grey[400], width: 1.5)),
+                child: Column(
+                  children: [
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      margin: EdgeInsets.only(left: 5.0),
+                      child: Text(
+                        'Pizza Margherita',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18.0,
+                          letterSpacing: 1.2,
+                        ),
+                      ),
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      margin: EdgeInsets.only(left: 5.0),
+                      child: rate(s: 4),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,143 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:food_app/homepage.dart';
+
+void main() {
+  runApp(MaterialApp(
+      title: 'Food App',
+      debugShowCheckedModeBanner: false,
+      color: Colors.green,
+      home: MyApp()));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+            backgroundColor: Colors.white,
+            body: Form(
+                key: _formkey,
+                child: ListView(
+                  children: <Widget>[
+                    Container(
+                      margin: EdgeInsets.only(top: 30.0, bottom: 50.0),
+                      child: Center(
+                        child: Image(image: AssetImage('images/foodlogo.png'),height: MediaQuery.of(context).size.height/7,),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    Container(
+                      alignment: Alignment.centerLeft,
+                      margin: EdgeInsets.only(left: 25.0),
+                      child: Text(
+                        'Sign Up Free Account',
+                        style: TextStyle(
+                          fontSize: 28.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25.0,
+                    ),
+                    Column(
+                      children: <Widget>[
+                        fieldInput(
+                          type: 'User Name',
+                          texttype: TextInputType.text,
+                        ),
+                        SizedBox(height: 20.0,),
+                        fieldInput(
+                            type: 'Email',
+                            texttype: TextInputType.emailAddress),
+                        SizedBox(height: 20.0,),
+                        fieldInput(
+                            type: 'Password',
+                            texttype: TextInputType.visiblePassword),
+                        SizedBox(height: 20.0,),
+                        fieldInput(
+                            type: 'Phone', texttype: TextInputType.number)
+                      ],
+                    ),
+                    SizedBox(
+                      height: 27.0,
+                    ),
+                    Container(
+                      margin: EdgeInsets.only(left: 50.0, right: 50.0),
+                      child: RaisedButton(
+                        color: Colors.deepPurpleAccent,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(30.0)),
+                        child: Text(
+                          'Sign UP',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20.0,
+                            color: Colors.white,
+                          ),
+                        ),
+                        onPressed: () {
+                          if(_formkey.currentState.validate()){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) {
+                              return HomePage();
+                            }),
+                          );
+                        }},
+                        elevation: 0.0,
+                      ),
+                    )
+                  ],
+                ))));
+  }
+
+  Widget fieldInput({String type, TextInputType texttype}) => Container(
+        margin: EdgeInsets.only(left: 20.0, right: 20.0, bottom: 7.7),
+        child: Center(
+          child: TextFormField(
+            validator: (String value) {
+              if (value.isEmpty) {
+                return 'Please enter the data';
+              }
+              return null;
+            },
+            decoration: InputDecoration(
+                filled: true,
+                fillColor: Colors.grey[300],
+                enabledBorder: const OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                  borderSide: const BorderSide(color: Colors.grey, width: 0.0),
+                ),
+                labelText: '$type',
+                labelStyle: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey[600],
+                    letterSpacing: 2.0,
+                    fontSize: 22.0),
+                hintText: '$type',
+                hintStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey,
+                  fontSize: 20.0,
+                ),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                )),
+            keyboardType: texttype,
+          ),
+        ),
+      );
+}
